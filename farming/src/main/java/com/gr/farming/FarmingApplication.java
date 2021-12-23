60b109c5eac4d482a0fed0e86662dba0e3165e2c
PEOPLE <PEOPLE@DESKTOP-VR3IDQC>
2021-12-24 01:19:48 +0900

    인덱스 테스트

--- farming/src/main/java/com/gr/farming/FarmingApplication.java
+++ farming/src/main/java/com/gr/farming/FarmingApplication.java
@@ -1,9 +1,12 @@
 package com.gr.farming;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class FarmingApplication {
 
 	public static void main(String[] args) {
