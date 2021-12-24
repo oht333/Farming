c305820fb06c4c275b7eee1b902e8d3b5f3a3813
ziwooooo <95212806+ziwooooo@users.noreply.github.com>
2021-12-24 16:52:31 +0900

    로그인 구현중

--- farming/src/main/java/com/gr/farming/member/controller/LoginController.java
+++ farming/src/main/java/com/gr/farming/member/controller/LoginController.java
@@ -1,13 +1,21 @@
 package com.gr.farming.member.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.gr.farming.member.model.MemberService;
+import com.gr.farming.member.model.MemberVO;
 
 @Controller
 @RequestMapping("/login")
@@ -23,7 +31,16 @@ public class LoginController {
 	}
 	
 	@GetMapping("/login")
-	public void login() {
+	public void login_get() {
 		logger.info("로그인 화면");
+	}
+	@PostMapping
+	public String login_post(@ModelAttribute MemberVO vo, @RequestParam(required = false) String chkSave,
+			HttpServletRequest request, HttpServletResponse response, Model model) {
+		logger.info("로그인 처리 vo={}",vo);
+		
+		String msg = "로그인 처리 실패", url = "/login/login";
+		
+		int result = service
 	}
 }
