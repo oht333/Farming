7a1ebe3c0432f0faff4324e89fdf9a3309ae7b25
yumi-oh <58407203+yumi-oh@users.noreply.github.com>
2021-12-30 03:36:18 +0900

    견적서 DB연동 완료! 질문/답 테이블 분리하고 수정해야함.

--- farming/src/main/java/com/gr/farming/request/controller/RequestController.java
+++ farming/src/main/java/com/gr/farming/request/controller/RequestController.java
@@ -1,19 +1,20 @@
 package com.gr.farming.request.controller;
 
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.gr.farming.request.model.RequestDesignVO;
-import com.gr.farming.request.model.RequestDevelopVO;
+import com.gr.farming.request.model.RequestQnaVO;
 import com.gr.farming.request.model.RequestService;
 
 @Controller
@@ -23,42 +24,40 @@ public class RequestController {
 	private static final Logger logger
 		= LoggerFactory.getLogger(RequestController.class);
 	
-	public final RequestService requestService;
+	private final RequestService requestService;
 	
 	@Autowired
 	public RequestController(RequestService requestService) {
 		this.requestService = requestService;
 	}
-
 	/*
-	 * @RequestMapping("/request") public void request(@RequestParam(required =
-	 * false) int categoryNo, Model model ) {
-	 * logger.info("견적서요청 메인화면 보여주기, 파라미터 categoryNo={}", categoryNo);
-	 * 
-	 * model.addAttribute("categoryNo",categoryNo); }
-	 */
 	@RequestMapping("/request")
-	public void request( ) {
-		
+	public void request() {
 		logger.info("견적서 작성 메인화면 보여주기");
-
+	}
+	*/
+	
+	@RequestMapping("/request")
+	public void request(@RequestParam int categoryNo, Model model) {
+		logger.info("견적서 작성 메인화면 보여주기, 파라미터 categoryNo={}", categoryNo);
+		
+		model.addAttribute("categoryNo",categoryNo);
+	}
+	
+	@GetMapping("/requestWrite")
+	public String request_post(@RequestParam int categoryNo, Model model) {
+		
+		logger.info("견적서 작성 메인화면 보여주기, 파라미터 categoryNo={}", categoryNo);
+	
+		List<RequestQnaVO> list=requestService.selectByCategoryNo(categoryNo);
+		logger.info("견적서 문답 조회 결과, list.size={}", list.size());
+		
+		model.addAttribute("list", list);
+		model.addAttribute("categoryNo",categoryNo);
+		
+		return "request/request_1";
 	}
 	/*
-	 * @GetMapping("/develop/request1") public String request1_get(@RequestParam int
-	 * categoryNo, Model model) {
-	 * 
-	 * logger.info("견적서 작성 화면 보여주기, 파라미터 categoryNo={}", categoryNo);
-	 * 
-	 * List<RequestQnaVO> list=requestService.selectByCategoryNo(categoryNo);
-	 * logger.info("견적서 작성을 위한 문답 list.size={}", list.size());
-	 * 
-	 * model.addAttribute("list", list); logger.info("list={}",list);
-	 * 
-	 * return "request/develop/request1";
-	 * 
-	 * }
-	 */
-	
 	@GetMapping("/requestWrite/develop")
 	public String requestDevelop() {
 		
@@ -115,7 +114,7 @@ public class RequestController {
 		model.addAttribute("url", url);
 		
 		return "common/message";
-	}
+	}*/
 	
 	@GetMapping("/request_success")
 	public String request_success() {
