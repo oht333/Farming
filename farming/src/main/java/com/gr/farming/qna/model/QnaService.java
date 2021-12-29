2c8d12ff1633c397efe934b4bf8e21ff574c6cc2
JeeMoon <87937817+JeeMoon@users.noreply.github.com>
2021-12-30 02:20:04 +0900

    12월30일 큐엔에이 리스트 수정중

--- farming/src/main/java/com/gr/farming/qna/model/QnaService.java
+++ farming/src/main/java/com/gr/farming/qna/model/QnaService.java
@@ -1,6 +1,11 @@
 package com.gr.farming.qna.model;
 
+import java.util.List;
+
+import com.gr.farming.common.SearchVO;
 
 public interface QnaService {
 	public int insertQna(QnaVO vo);
+	public List<QnaVO> selectAll(SearchVO searchVO);
+	int selectTotalRecord(SearchVO searchVo);
 }
